d1c0bf13c67434754ba48d2ddba1f667fb1e547e
luky022 <46733875+luky022@users.noreply.github.com>
2024-12-05 13:44:43 -0300

    Add files via upload

---
+++ sql 05.sql
@@ -0,0 +1,122 @@
+-- MySQL dump 10.13  Distrib 8.0.34, for Win64 (x86_64)
+--
+-- Host: 127.0.0.1    Database: gestor-financeiro
+-- ------------------------------------------------------
+-- Server version	5.5.5-10.4.32-MariaDB
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `categorias`
+--
+
+DROP TABLE IF EXISTS `categorias`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `categorias` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `categoriasnome` varchar(250) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `categorias`
+--
+
+LOCK TABLES `categorias` WRITE;
+/*!40000 ALTER TABLE `categorias` DISABLE KEYS */;
+INSERT INTO `categorias` VALUES (5,'alimentação'),(6,'diversão');
+/*!40000 ALTER TABLE `categorias` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `despesas`
+--
+
+DROP TABLE IF EXISTS `despesas`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `despesas` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `data` date NOT NULL,
+  `descricao` varchar(250) NOT NULL,
+  `despesavalor` varchar(250) NOT NULL,
+  `categoria` varchar(250) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=32 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `despesas`
+--
+
+LOCK TABLES `despesas` WRITE;
+/*!40000 ALTER TABLE `despesas` DISABLE KEYS */;
+INSERT INTO `despesas` VALUES (15,'2024-10-11','2','2222',''),(17,'2024-10-18','22222222222222','2222222222',''),(18,'2024-03-26','mes 3','2323',''),(19,'2024-01-10','internet','90',''),(20,'2024-02-21','compras do mês','400',''),(26,'2024-11-28','internet','90','diversão'),(28,'0000-00-00','internet','90','diversão'),(29,'0000-00-00','internet','90','diversão'),(30,'0000-00-00','','','alimentação'),(31,'2024-12-04','internet','90','alimentação');
+/*!40000 ALTER TABLE `despesas` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `receitas`
+--
+
+DROP TABLE IF EXISTS `receitas`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `receitas` (
+  `receitavalor` varchar(300) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `receitas`
+--
+
+LOCK TABLES `receitas` WRITE;
+/*!40000 ALTER TABLE `receitas` DISABLE KEYS */;
+INSERT INTO `receitas` VALUES ('1000');
+/*!40000 ALTER TABLE `receitas` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `salario`
+--
+
+DROP TABLE IF EXISTS `salario`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `salario` (
+  `salariovalor` varchar(350) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `salario`
+--
+
+LOCK TABLES `salario` WRITE;
+/*!40000 ALTER TABLE `salario` DISABLE KEYS */;
+INSERT INTO `salario` VALUES ('1412');
+/*!40000 ALTER TABLE `salario` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2024-12-05 13:08:34
