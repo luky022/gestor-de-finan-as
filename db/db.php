d1d6cacb73483f82a452621cfa38684be86a1f27
luky022 <46733875+luky022@users.noreply.github.com>
2024-12-05 13:39:12 -0300

    Create db.php

---
+++ db/db.php
@@ -0,0 +1,38 @@
+<?php 
+
+
+$hostname = "localhost";
+$bancodedados = "gestor-financeiro";
+$usuario = "root";
+$senha = "";
+
+$conexao = new mysqli($hostname, $usuario, $senha, $bancodedados);
+
+/*
+ if ($conexao->connect_errno) {
+    echo "erro de conexão";
+} else {
+    echo "conectado com sucesso";
+}
+
+
+  */
+
+
+
+
+
+
+
+
+
+?>
+
+
+
+
+
+
+
+
+
